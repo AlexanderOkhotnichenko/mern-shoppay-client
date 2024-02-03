d681e4d0c52b31cd5b2bc4f805dcf2b274d7e0a9
Alexander <shashaohota@gmail.com>
2024-02-03 18:50:37 +0200

    fix images

--- src/components/NoResult/NoResult.jsx
+++ src/components/NoResult/NoResult.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import no_result from "/src/assets/img/noresult.webp"
 import styles from "./noresult.module.scss";
 
 export function NoResult() {
   return (
     <div className={styles.no_result}>
-      <img src="/src/assets/img/noresult.webp" alt={"No Result"} className={styles.no_result__hero} />
+      <img src={no_result} alt={"No Result"} className={styles.no_result__hero} />
       <h1 className={styles.no_result__title}>Sorry, we couldn't find any results</h1>
     </div>
   )
