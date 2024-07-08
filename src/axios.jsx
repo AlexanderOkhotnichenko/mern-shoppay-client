32662ba1648f23e1a123cc9bc69e7824a6356418
Alexander Okhotnichenko <104731043+AlexanderOkhotnichenko@users.noreply.github.com>
2024-07-08 10:22:00 +0300

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://mern-shoppay.onrender.com',
   headers: {
     Authorization: '',
     'Content-Type': 'application/json',
